c73fd88d67a4247d5a83df1ba82918652943e137
DESKTOP-G3KR89E\AlexC <alexchin1999@outlook.com>
2025-09-09 17:33:32 -0400

    implementing trip cards and header layout

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router"
 import {SidebarComponent} from "@syncfusion/ej2-react-navigations"
+import MobileSidebar from "components/MobileSidebar"
 import NavItems from "components/NavItems"
 
 const AdminLayout = () => {
   return (
     <div className='admin-layout'>
-        MobileSidebar
+        <MobileSidebar/>
         <aside className="w-full max-w-[270px] hidden lg:block"> Sidebar
             <SidebarComponent width={270} enableGestures={false}> 
                 <NavItems />
--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,6 +1,14 @@
+import Header from "components/Header"
+
 const AllUsers = () => {
   return (
-    <div>All Users</div>
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Checkout our current users in real time"
+      />
+      All users Page contents
+    </main>
   )
 }
 
