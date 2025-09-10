118e02d5a20f53a1eb6a02eb1c3a6ae50bd76909
christinecnwang <christinecnwang@gmail.com>
2025-09-09 23:57:06 -0400

    add appwrite support, auth functionality and UI

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,23 +1,47 @@
-import { Outlet } from "react-router"
-import {SidebarComponent} from "@syncfusion/ej2-react-navigations"
-import MobileSidebar from "components/MobileSidebar"
-import NavItems from "components/NavItems"
+import { Outlet, redirect } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import MobileSidebar from "components/MobileSidebar";
+import NavItems from "components/NavItems";
+import { account } from "~/appwrite/client";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
+
+export async function clientLoader() {
+  try {
+    const user = await account.get();
+
+    if (!user.$id) return redirect("/sign-in");
+
+    const existingUser = await getExistingUser(user.$id);
+
+    if (existingUser?.status === "user") {
+      return redirect("/");
+    }
+
+    return existingUser?.$id ? existingUser : await storeUserData();
+  } catch (error) {
+    console.log("Error in clientLoader", error);
+    return redirect("/sign-in");
+  }
+}
 
 const AdminLayout = () => {
   return (
     <div className='admin-layout'>
-        <MobileSidebar/>
-        <aside className="w-full max-w-[270px] hidden lg:block"> Sidebar
-            <SidebarComponent width={270} enableGestures={false}> 
-                <NavItems />
+      <MobileSidebar />
 
-            </SidebarComponent>
-        </aside>
-        <aside className="children"> 
-            <Outlet />
-        </aside>
+      <aside className='w-full max-w-[270px] hidden lg:block'>
+        {" "}
+        Sidebar
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
+
+      <aside className='children'>
+        <Outlet />
+      </aside>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
